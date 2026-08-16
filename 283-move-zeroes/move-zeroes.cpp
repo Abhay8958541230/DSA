ae3f537bf2f0d00dea1118daea77f87865551253
Abhay Pratap Rathore <145528453+Abhay8958541230@users.noreply.github.com>
2026-08-16 16:21:56 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.72%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZero = 0;
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                nums[lastNonZero++] = nums[i];
+            }
+        }
+
+        // Fill the rest with zeros
+        for (int i = lastNonZero; i < nums.size(); ++i) {
+            nums[i] = 0;
+        }
+    }
+};
